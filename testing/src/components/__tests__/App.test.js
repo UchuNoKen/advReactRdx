286ba9c82ef6b7f9e84566ff145efcd26075e4e8
UchuNoKen <xen.o.fite@gmail.com>
2019-04-26 11:41:00 -0400

    it block comments

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 import App from "../App";
 
 it("shows a comment box", () => {
-  const div = document.create("div");
+  // JSDOM creates a fake div
+  // provides document.xxx object
+  const div = document.createElement("div");
 
+  // React sends HTML from component to div
+  // created by JSDOM
   ReactDOM.render(<App />, div);
 
+  // Looks inside div to see if CommentBox is there
+
+  // Look at div, find App component and remove
+  // it, performing cleanup
   ReactDOM.unmountComponentAtNode(div);
 });
